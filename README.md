f0774d4f3322aac9a76488e96b2774ba2e3eef0d
nathancox <me@nathan.net.nz>
2012-04-14 23:13:26 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+SilverStripe TextFieldGroup
+===================================
+
+This is intended to make any combination of text fields and labels on a single line.  Eg for URLSegments prepended by the domain as a label, or for X, Y and Z coordinates for a single location.
+
+The differences from FieldGroup are
+a) it takes away the minimum width of fields so things are spaced approriately; and
+b) if you pass in a string instead of a field it's automatically turned in to a LiteralField
+
+No dramatic functionality, just a convenience class for formatting.
+
+Currently being developed on SilverStripe 3, not tested on 2.x yet.
+
+Maintainer Contacts
+-------------------
+* Nathan Cox (<nathan@flyingmonkey.co.nz>)
+
+Requirements
+------------
+* SilverStripe 3.0+
+
+Documentation
+-------------
+[GitHub](https://github.com/nathancox/silverstripe-tagitfield)
+
+Installation Instructions
+-------------------------
+
+1. Place the files in a directory called tagitfield in the root of your SilverStripe installation
+2. Visit yoursite.com/dev/build to rebuild the database
+
+Usage Overview
+--------------
+
+
+$fields->addFieldToTab('Root.Main', new TextFieldGroup('Map coordinates',
+		$this->getMapLink() . '/',
+		new NumericField("LocationX", "", "", 3),
+		"/",
+		new NumericField("LocationY", "", "", 3)
+));
+
+$fields->addFieldToTab('Root.Main', new TextFieldGroup('Subdomain',
+		'http://',
+		new TextField("Subdomain", ""),
+		'.example.com'
+));
+
+
+Known Issues
+------------
+[Issue Tracker](https://github.com/nathancox/silverstripe-tagitfield/issues)
---
+++ code/TextFieldGroup.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This is intended to make any combination of text fields and labels on a single line.  Eg for URLSegments prepended by the domain as a label, or for X, Y and Z coordinates for a single location.
+ * 
+ * The differences from FieldGroup are
+ * a) it takes away the minimum width of fields so things are spaced approriately; and
+ * b) if you pass in a string instead of a field it's automatically turned in to a LiteralField
+ * 
+ */
+class TextFieldGroup extends FieldGroup {
+	
+	function __construct($arg1 = null, $arg2 = null) {
+		if (is_array($arg1) || is_a($arg1, 'FieldSet')) {
+			$fields = $arg1;
+		} else if (is_array($arg2) || is_a($arg2, 'FieldList')) {
+			$this->title = $arg1;
+			$fields = $arg2;
+		} else {
+			$fields = func_get_args();
+			if(!is_object(reset($fields))) $this->title = array_shift($fields);
+		}
+		
+		foreach($fields as $key => $field) {
+			if (is_string($field)) {
+				$fields[$key] = new LiteralField($this->Name().'Literal'.$key, $field);
+			}
+		}
+					
+		parent::__construct($fields);
+	}
+	
+	function Field() {
+		Requirements::css('textfieldgroup/css/TextFieldGroup.css');
+		
+		$fs = $this->FieldList();
+    $idAtt = isset($this->id) ? " id=\"{$this->id}\"" : '';
+		$content = "<div class=\"textfieldgroup\"$idAtt>";
+
+		$count = 1;
+		foreach($fs as $subfield) {
+
+			//label the first and last fields of each surrounding div
+			if ($count == 1) $firstLast = "first";
+			elseif ($count == count($fs)) $firstLast = "last";
+			else $firstLast = '';
+
+			$content .= "<div class=\"textfieldgroup-field $firstLast\">" . $subfield->{$this->subfieldParam}() . "</div>";
+			$count++;
+		}
+		$content .= "</div>";
+		
+		return $content;
+	}
+	
+}
